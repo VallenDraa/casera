23a0044a83b181f5e0c935cba02809cb40808362
KoooYoung <Jestinevallendradwiputra@gmail.com>
2022-04-25 21:42:44 +0700

    applied recipe API'ss, userpage slidesstill buggy

--- server/api/recipe.js
+++ server/api/recipe.js
@@ -1,18 +1,68 @@
 const User = require('../model/User');
+const axios = require('axios').default;
 
 // recipe related api
+
+const isSaved = async (req, res) => {
+  const { username, idMeal } = req.query;
+
+  // return if there is no username of id meal
+  if (!username || !idMeal) return;
+
+  try {
+    const user = await User.findOne({ username });
+    const containIdMeal = user.savedRecipes.includes(idMeal);
+
+    res.status(200).json({ ok: true, code: 200, containIdMeal, idMeal });
+  } catch (e) {
+    res.status(500).json({ ok: false, code: 500, e });
+  }
+};
+
+const getSavedRecipes = async (req, res) => {
+  try {
+    const user = await User.findById(req.query._id);
+    const { username, savedRecipes } = user;
+
+    // get recipes from mealdb
+    fetchFromMealDB(savedRecipes)
+      .then((savedRecipes) => {
+        res.status(200).json({
+          code: 200,
+          ok: true,
+          getRecipe: true,
+          username,
+          savedRecipes,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ code: 500, ok: false, getRecipe: false });
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ code: 500, ok: false, getRecipe: false });
+  }
+};
+
 const saveRecipe = async (req, res) => {
   const { _id, idMeal } = req.body;
 
   try {
     const user = await User.findById(_id);
+
     // check if user haven't save the recipe
     if (!user.savedRecipes.includes(idMeal)) {
       user.savedRecipes.push(idMeal);
       user
         .save()
         .then(() =>
-          res.status(200).json({ code: 200, ok: true, recipeSaved: true })
+          res.status(200).json({
+            code: 200,
+            ok: true,
+            recipeSaved: true,
+            currentSavedRecipes: user.savedRecipes,
+          })
         )
         .catch(() =>
           res.status(500).json({ code: 500, ok: false, recipeSaved: false })
@@ -30,35 +80,8 @@ const saveRecipe = async (req, res) => {
   }
 };
 
-const isSaved = async (req, res) => {
-  const { username, idMeal } = req.query;
-
-  // return if there is no username of id meal
-  if (!username || !idMeal) return;
-
-  try {
-    const user = await User.findOne({ username });
-    const containIdMeal = user.savedRecipes.includes(idMeal);
-
-    res.status(200).json({ ok: true, code: 200, containIdMeal, idMeal });
-  } catch (e) {
-    res.status(500).json({ ok: false, code: 500 });
-  }
-};
-
-const getSavedRecipes = async (req, res) => {
-  try {
-    const user = await User.findById(req.query._id);
-    const { username, savedRecipes } = user;
-    res
-      .status(200)
-      .json({ code: 200, ok: true, getRecipe: true, username, savedRecipes });
-  } catch (err) {
-    res.status(500).json({ code: 500, ok: false, getRecipe: false });
-  }
-};
-
 const removeSavedRecipe = async (req, res) => {
+  // console.log(req.body);
   const { _id, idMeal } = req.body;
   try {
     const user = await User.findById(_id);
@@ -67,33 +90,58 @@ const removeSavedRecipe = async (req, res) => {
     // check if user saved the recipe
     if (user.savedRecipes.includes(idMeal)) {
       user.savedRecipes = user.savedRecipes.filter(
-        (recipeID) => recipeID !== idMeal
+        (recipeID) => recipeID !== Number(idMeal)
       );
+
       user
         .save()
         .then(() =>
           res.status(200).json({
             code: 200,
             ok: true,
-            removeRecipe: true,
+            recipeRemoved: true,
             username,
-            savedRecipes: user.savedRecipes,
+            currentSavedRecipes: user.savedRecipes,
           })
         )
         .catch(() =>
-          res.status(500).json({ code: 500, ok: false, removeRecipe: false })
+          res.status(500).json({ code: 500, ok: false, recipeRemoved: false })
         );
     } else {
       res.status(404).json({
         code: 404,
         ok: true,
-        removeRecipe: false,
+        recipeRemoved: false,
         msg: "User Hasn't Save This Recipe",
       });
     }
   } catch (err) {
-    res.status(500).json({ code: 500, ok: false, removeRecipe: false });
+    res.status(500).json({ code: 500, ok: false, recipeRemoved: false });
   }
+};
+
+// getSavedRecipes supporting function
+const fetchFromMealDB = async (ids) => {
+  const result = [];
+
+  for (let i = 0; i < ids.length; i++) {
+    // return if index is more than 4
+    if (i > 5) return result;
+
+    try {
+      const { data } = await axios({
+        method: 'GET',
+        url: `http://www.themealdb.com/api/json/v1/1/lookup.php?i=${ids[i]}`,
+        responseType: 'json',
+      });
+
+      const { strMeal, idMeal, strMealThumb } = data.meals[0];
+      result.push({ strMeal, idMeal, strMealThumb });
+    } catch (e) {
+      return e;
+    }
+  }
+  return result;
 };
 
 module.exports = {
