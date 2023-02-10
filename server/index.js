04607934568d2d105a730b9248e8729bfaade345
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-10 11:20:56 +0700

    chore: remove unused code

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ const apiRecipeRouter = require('./routes/apiRecipeRoutes');
 const apiUserRouter = require('./routes/apiUserRoutes');
 const apiRatingRouter = require('./routes/apiRatingRoute');
 const authRouter = require('./routes/authRoutes');
-// const https = require('https');
 
 // allow json
 app.use(express.json({ limit: '500kb' }));
 
 // connect to database
+mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGOSERVER)
   .finally(() => console.log('connected to database'));
@@ -21,6 +21,5 @@ app.use('/api', apiRecipeRouter);
 app.use('/api', apiUserRouter);
 app.use('/api', apiRatingRouter);
 app.use('/auth', authRouter);
-// console.log(https);
 
 app.listen(process.env.PORT || 3001, () => console.log('listening on 3001'));
